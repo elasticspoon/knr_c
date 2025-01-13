cc7a94c5aef78cf8b4268aa218ec0c613bc57adc
elasticspoon <mail@yuribocharov.dev>
2025-01-12 19:15:45 -0500

    feat: reverse order

--- c_1/celcius.c
+++ c_1/celcius.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 
 int main() {
-  int min = 0;
-  int max = 300;
-  int step = 20;
-
   // formula: C = 5/9 * (F - 32)
   // formula: F = (C / 5) * 9 + 32
   printf("Celsius\tFarenheit\n");
-  while (min <= max) {
-    float f = (min / 5.0) * 9.0 + 32.0;
-    printf("%7d\t%9.1f\n", min, f);
-    min += step;
+  for (int c = 300; c >= 0; c -= 20) {
+    float f = (c / 5.0) * 9.0 + 32.0;
+    printf("%7d\t%9.1f\n", c, f);
   }
 }
