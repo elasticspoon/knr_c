9ea4e108f34ed11ab42243a57a3bb240986fcd88
elasticspoon <mail@yuribocharov.dev>
2025-02-15 19:02:17 -0500

    feat: ex 1_15 use a funciton

--- c_1/farenheit.c
+++ c_1/farenheit.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+
+float farenheit_to_celcius(int f);
+
 int main() {
   // formula: C = 5/9 * (F - 32)
   printf("Farenheit\tCelsius\n");
   for (int f = 0; f <= 300; f += 20) {
-    float c = ((f - 32) * 5.0) / 9.0;
+    float c = farenheit_to_celcius(f);
     printf("%9d\t%7.2f\n", f, c);
   }
 }
+
+float farenheit_to_celcius(int f) { return ((f - 32) * 5.0) / 9.0; }
